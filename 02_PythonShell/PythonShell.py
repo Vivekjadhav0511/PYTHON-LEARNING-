bc789fe3f168a45472f6eae7627415994ab8afad
Vivek Jadhav <vivekjadhav0511@gmail.com>
2024-11-21 12:53:41 +0530

    Python Introduction

--- 02_PythonShell/PythonShell.py
+++ 02_PythonShell/PythonShell.py
@@ -7,20 +7,3 @@ book("How You lost Me !!")
 
 book_one = "Gita"
 book_two = "Art Of Not "
-
-""" 
-import hello_01
-Hello Pyhton
-Python Function !!
->>> hello_01.one("called")
-called
->>> from importlib import reload
->>> reload(hello_01)
-How You lost Me !!
-<module 'hello_01' from 'D:\\GitHub0511\\JavaScript\\PYTHON-LEARNING-\\01_BasicsOfPython\\hello_01.py'>      
->>> hello_01.book_one
-'Gita'
->>> hello_01.book_two
-'Art Of Not '
->>>
- """
