4bf50f46ae548d782d0ddac1e7d7d210c3800dfa
Vivekjadhav0511 <vivekjadhav8411@gmail.com>
2024-11-21 23:42:15 +0530

    mutable&Immutable In Python

--- 03_Mutable&Imutable/mutable&Immutable.py
+++ 03_Mutable&Imutable/mutable&Immutable.py
@@ -1,1 +1,23 @@
-# 
+# mutable&Immutable
+  
+usernam = "vivek jadhav"
+print(usernam)
+usernam = "userName Has Been Changed"
+print(usernam)
+
+""" 
+Mutable
+A variable or object that can be changed after it's created. For example, you can add, remove, or modify the contents of a mutable list or dictionary.
+Immutable
+A variable or object that cannot be changed after it's created. For example, you can't change the value of an immutable int, float, bool, complex, string, tuple, or set. 
+
+Here are some other differences between mutable and immutable objects in Python:
+
+# Memory usage: Mutable objects are more memory-intensive than immutable objects because 
+they store a copy of their contents whenever they are changed. 
+
+# Error-proneness: Immutable objects are less prone to errors than mutable objects. 
+
+# Creating new objects: When you change an immutable object, Python creates a new 
+object with a different memory allocation.  """
+
