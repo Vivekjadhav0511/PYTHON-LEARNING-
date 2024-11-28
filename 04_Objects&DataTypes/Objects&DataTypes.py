1e01c8393e21e8b29f6d86629c279369c1351240
Vivekjadhav0511 <vivekjadhav8411@gmail.com>
2024-11-28 23:34:35 +0530

    DataTypes In Python

---
+++ 04_Objects&DataTypes/Objects&DataTypes.py
@@ -0,0 +1,38 @@
+# Objects And DataTypes 
+
+""" Mutable 
+Values can be modified => Examples: list, dict, set
+
+Immutable
+Values cannot be modified => Examples: int, str, tuple """
+
+Numbers = 1234
+
+String = "Vivek Jadhav"
+
+myList = [10,20,40,60,] 
+print(myList)
+
+tuples = (2,"spam",6,6,99,35)
+
+dictionary = {  
+    'name':'Vivek',
+    'age': 41
+}  
+#   key value pair => dict
+
+set = set('abc')
+
+file = open('myFile.txt')
+
+boolean = True
+
+None = None 
+
+# functions , modules , classes 
+
+# ADVANCE => decorater , Generator , itarator 
+
+
+
+
